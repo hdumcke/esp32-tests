bfdc52226c1c266440180d1e37b1c3b34050e259
Horst Dumcke <hdumcke@marlysys.com>
2023-01-01 16:10:12 +0100

    merged changes from pat92fr manually

--- esp32_proxy/esp32-proxy.cpp
+++ esp32_proxy/esp32-proxy.cpp
@@ -17,68 +17,112 @@
 #include "mini_pupper_host_base.h"
 #include "mini_pupper_protocol.h"
 
-#define N 12
+static bool const print_debug     {false};
+static bool const print_debug_max {false};
 
-static void *control_block;
+static char const * filename {"/dev/ttyAMA1"};
 
-void esp32_protocol(void *control_block)
+// Setpoint and feedback data format for client-server communication (PoD)
+struct setpoint_and_feedback_data
 {
-    static const char* filename = "/dev/ttyAMA1";
-    int fd;
-    int rc;
-    int offset;
-    struct termios *tty;
-    bool print_debug;
-    print_debug = false;
+    parameters_control_instruction_format control;
+    parameters_control_acknowledge_format feedback;
+};
 
-    // Initialize positions to 0
-    u16 neutral_pos;
-    neutral_pos = 512;
-    for(size_t servo_index=0; servo_index<N; ++servo_index)
+// Task handling communication with ESP32
+// - parameter (input/output) : the client/server shared-memory buffer
+void esp32_protocol(setpoint_and_feedback_data * control_block)
+{
+    // Check control block once
+    if(control_block==NULL) exit(EXIT_FAILURE);
+    
+    // Initialize all control/goal_position to neutral
+    u16 const neutral_pos {512};
+    for(auto & goal_position : control_block->control.goal_position)
     {
-        offset = servo_index * 2;
-        memcpy(control_block + offset, &neutral_pos, 2);
+        goal_position = neutral_pos;
     }
 
-    fd = open(filename, O_RDWR | O_NOCTTY);
-    if (fd < 0) {
+    // reference : https://www.pololu.com/docs/0J73/15.5
+
+    // Open serial device
+    int fd = open(
+        filename,           // UART device
+        O_RDWR | O_NOCTTY   // Read-Write access + Not the terminal of this process
+    );
+    if (fd < 0)
+    {
         printf("%s: failed to open UART device\n", __func__);
         exit(EXIT_FAILURE);
     }
-    tty = (termios *)malloc(sizeof(*tty));
-    if (!tty) {
+
+    // Flush away any bytes previously read or written.
+    int result = tcflush(fd, TCIOFLUSH);
+    if (result)
+    {
+        printf("%s: failed to flush\r\n", __func__);
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Get the current configuration of the serial port.
+    struct termios options;
+    result = tcgetattr(fd, &options);
+    if (result)
+    {
         printf("%s: failed to allocate UART TTY instance\n", __func__);
+        close(fd);
         exit(EXIT_FAILURE);
     }
 
-    memset(tty, 0, sizeof(*tty));
-    tty->c_iflag |=  IGNPAR;
-    tty->c_cflag =  CS8 | CREAD | B3000000;
-    tty->c_cc[VTIME] = 10; // Set timeout of 1.0 seconds
-    tty->c_cc[VMIN] = 0;
+    // note : Les fonctions termios établissent une interface générale 
+    //        sous forme de terminal, permettant de contrôler les ports 
+    //        de communication asynchrone.  
 
-    tcflush(fd, TCIFLUSH);
-    rc = tcsetattr(fd, TCSANOW, tty);
-    if (rc) {
+    // Turn off any options that might interfere with our ability to send and
+    // receive raw binary bytes.
+    options.c_iflag &= ~(INLCR | IGNCR | ICRNL | IXON | IXOFF); // IGNPAR ?
+    options.c_oflag &= ~(ONLCR | OCRNL);
+    options.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+
+    // Set up timeouts: Calls to read() will return as soon as there is
+    // at least one byte available or when 100 ms has passed.
+    options.c_cc[VTIME] = 1;
+    options.c_cc[VMIN] = 0;
+
+    // Set baud rate
+    cfsetospeed(&options, B3000000);
+
+    // Apply options
+    result = tcsetattr(fd, TCSANOW, &options);
+    if (result)
+    {
         printf("%s: failed to set attributes\r\n", __func__);
+        close(fd);
         exit(EXIT_FAILURE);
     }
 
+    // control-loop
     for (;;)
     {
+        // heart-beat
+        if (print_debug_max)
+        {
+            printf(".");
+        }
+
+        // Flush away any bytes previously read or written.
+        int result = tcflush(fd, TCIOFLUSH);
+        if (result)
+        {
+            printf("%s: failed to flush\r\n", __func__);
+            close(fd);
+            exit(EXIT_FAILURE);
+        }
+
         /*
          * Encode a CONTROL frame
          */
-
-        // Build parameters
-        parameters_control_instruction_format parameters;
-
-        // Copy 12 servo goal positions into the goal_position array of parameters
-        for(size_t servo_index=0; servo_index<N; ++servo_index)
-        {
-            offset = servo_index * 2;
-            memcpy(&parameters.goal_position[servo_index], (char*)control_block + offset, 2);
-        }
 
         // Compute the size of the payload (parameters length + 2)
         size_t const tx_payload_length { sizeof(parameters_control_instruction_format) + 2 };
@@ -87,54 +131,75 @@ void esp32_protocol(void *control_block)
         size_t const tx_buffer_size { tx_payload_length + 4 };
 
         // Build the frame
-        u8 tx_buffer[tx_buffer_size] {
+        u8 tx_buffer[tx_buffer_size]
+        {
             0xFF,               // header
             0xFF,               // header
             0x01,               // default ID
             tx_payload_length,  // length
             INST_CONTROL        // instruction
         };
-        memcpy(tx_buffer+5,&parameters,sizeof(parameters_control_instruction_format));
+        memcpy(tx_buffer+5,&control_block->control,sizeof(parameters_control_instruction_format));
 
         // Checksum
         tx_buffer[tx_buffer_size-1] = compute_checksum(tx_buffer);
 
         // Send
-        int result = write(fd, (char *)tx_buffer, tx_buffer_size);
-        if (print_debug)
-	{
-	    printf("uart writen:%d\n",result);
-	}
+        result = write(fd, (char *)tx_buffer, tx_buffer_size);
+        if(result != (ssize_t)tx_buffer_size)
+        {
+            printf("failed to write to port");
+            close(fd);
+            exit(EXIT_FAILURE);
+        }
+        if (print_debug_max)
+        {
+    	    printf("uart writen:%d\n",result);
+    	}
+
+        /*
+        result = tcdrain(fd);
+        if(result)
+        {
+            printf("failed to drain port");
+            close(fd);
+            exit(EXIT_FAILURE);
+        }
+        */
 
         // Read buffer
-	// If we do not get data within 1 second we assume ESP32 stopped sending and we send again
+    	// If we do not get data within 1 second we assume ESP32 stopped sending and we send again
         size_t const rx_buffer_size { 128 };
         u8 rx_buffer[rx_buffer_size] {0};
-	int read_length;
-	int expected_lenght;
-	expected_lenght = 4 + 1 + sizeof(parameters_control_acknowledge_format) + 1; // header + status + pos + load + chksum
-	offset = 0;
-	do {
-            read_length = read(fd, (char*)rx_buffer + offset, expected_lenght);
-	    if(!read_length)
-	    {
-		// time out
-		break;
-	    }
-	    expected_lenght -= read_length;
-	    offset += read_length;
-            if (print_debug)
+    	size_t const expected_lenght {4 + 1 + sizeof(parameters_control_acknowledge_format) + 1}; // header + status + pos + load + chksum
+        size_t received_length {0};
+    	while(received_length<expected_lenght)
+        {
+            ssize_t read_length = read(
+                fd,
+                (char*)(rx_buffer+received_length),
+                expected_lenght-received_length
+            );
+            if(read_length<0)
             {
-                printf("uart read:%d, waiting for %d\n",read_length, expected_lenght);
+                printf("failed to read from port");
+                close(fd);
+                exit(EXIT_FAILURE);
             }
-	} while (expected_lenght > 0);
+            if (print_debug_max)
+            {
+                printf("uart read:%lu, waiting for %lu/%lu\n",read_length, received_length,expected_lenght);
+            }
+            if(read_length==0)
+            {
+              // time out
+              break;
+            }
+    	    received_length += read_length;
+    	}
 
-	if(expected_lenght)
-	{
-	    // time out
-	    continue;
-	}
-
+        // not enough data received ?
+    	if(received_length<expected_lenght) continue;
 
         /*
          * Decode a CONTROL ACK frame
@@ -150,11 +215,9 @@ void esp32_protocol(void *control_block)
         {
             // log
             if (print_debug)
-	    {
+    	    {
                 printf("RX frame error : header invalid!\n");
-	    }
-            // flush RX FIFO
-            //// HOW TO LINUX ?
+    	    }
             // next
             continue;
         }
@@ -172,71 +235,54 @@ void esp32_protocol(void *control_block)
         {
             // log
             if (print_debug)
-	    {
-                printf("RX frame error : bad instruction [%d] or checksum [received:%d,expected:%d]!\n",rx_buffer[4],rx_buffer[rx_buffer[3]+3],expected_checksum);
-	    }
-            // flush RX FIFO
-            //// HOW TO LINUX ?
+    	    {
+                    printf("RX frame error : bad instruction [%d] or checksum [received:%d,expected:%d]!\n",rx_buffer[4],rx_buffer[rx_buffer[3]+3],expected_checksum);
+    	    }
             // next
             continue;
         }
 
         // decode parameters
-        parameters_control_acknowledge_format ack_parameters;
-        memcpy(&ack_parameters,rx_buffer+5,sizeof(parameters_control_acknowledge_format));
-        for(size_t servo_index=0; servo_index<N; ++servo_index)
-        {
-            offset = sizeof(parameters_control_instruction_format);
-            memcpy((char*)control_block + offset, &ack_parameters, sizeof(parameters_control_acknowledge_format));
-        }
+        memcpy(&control_block->feedback,rx_buffer+5,sizeof(parameters_control_acknowledge_format));
 
         // log
-
         if (print_debug)
-	{
+    	{
             printf("Present Position: %d %d %d %d %d %d %d %d %d %d %d %d\n",
-            ack_parameters.present_position[0],ack_parameters.present_position[1],ack_parameters.present_position[2],
-            ack_parameters.present_position[3],ack_parameters.present_position[4],ack_parameters.present_position[5],
-            ack_parameters.present_position[6],ack_parameters.present_position[7],ack_parameters.present_position[8],
-            ack_parameters.present_position[9],ack_parameters.present_position[10],ack_parameters.present_position[11]
+            control_block->feedback.present_position[0],control_block->feedback.present_position[1],control_block->feedback.present_position[2],
+            control_block->feedback.present_position[3],control_block->feedback.present_position[4],control_block->feedback.present_position[5],
+            control_block->feedback.present_position[6],control_block->feedback.present_position[7],control_block->feedback.present_position[8],
+            control_block->feedback.present_position[9],control_block->feedback.present_position[10],control_block->feedback.present_position[11]
             );
             printf("            Load: %d %d %d %d %d %d %d %d %d %d %d %d\n",
-            ack_parameters.present_load[0],ack_parameters.present_load[1],ack_parameters.present_load[2],
-            ack_parameters.present_load[3],ack_parameters.present_load[4],ack_parameters.present_load[5],
-            ack_parameters.present_load[6],ack_parameters.present_load[7],ack_parameters.present_load[8],
-            ack_parameters.present_load[9],ack_parameters.present_load[10],ack_parameters.present_load[11]
+            control_block->feedback.present_load[0],control_block->feedback.present_load[1],control_block->feedback.present_load[2],
+            control_block->feedback.present_load[3],control_block->feedback.present_load[4],control_block->feedback.present_load[5],
+            control_block->feedback.present_load[6],control_block->feedback.present_load[7],control_block->feedback.present_load[8],
+            control_block->feedback.present_load[9],control_block->feedback.present_load[10],control_block->feedback.present_load[11]
             );
 
-            printf("Attitude:  roll:%.3f  pitch:%.3f  yaw:%.3f\n", ack_parameters.roll, ack_parameters.pitch, ack_parameters.yaw);
-            printf("Power:  %.3fV  %.3fA\n", ack_parameters.voltage_V, ack_parameters.current_A);
-	}
+            printf("Attitude:  roll:%.3f  pitch:%.3f  yaw:%.3f\n", control_block->feedback.roll, control_block->feedback.pitch, control_block->feedback.yaw);
+            printf("Power:  %.3fV  %.3fA\n", control_block->feedback.voltage_V, control_block->feedback.current_A);
+	   }
     }
 }
 
 int main(int argc, char *argv[])
 {
-    struct sockaddr_un name;
-    int ret;
-    int pid;
-    int connection_socket;
-    int data_socket;
-    static u8 r_buffer[26];
-    static u8 s_buffer[26];
-    size_t const control_block_size { sizeof(parameters_control_instruction_format) +  sizeof(parameters_control_acknowledge_format)};
-
-    control_block = mmap(NULL, control_block_size, PROT_READ | PROT_WRITE,
-                         MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    memset(control_block, 0, control_block_size);
+    // allocate a shared-memory buffer for setpoint and feedback data exchange between clients and server
+    void * control_block = mmap(NULL, sizeof(setpoint_and_feedback_data), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    memset(control_block, 0, sizeof(setpoint_and_feedback_data));
 
     /* start UART protocol with ESP32 */
-    pid = fork();
-    if (pid == 0) {
-	esp32_protocol(control_block);
+    int pid = fork();
+    if (pid == 0)
+    {
+	   esp32_protocol(reinterpret_cast<setpoint_and_feedback_data*>(control_block));
     }
 
     /* Create local socket. */
 
-    connection_socket = socket(AF_UNIX, SOCK_SEQPACKET, 0);
+    int connection_socket = socket(AF_UNIX, SOCK_SEQPACKET, 0);
     if (connection_socket == -1) {
         perror("socket");
         exit(EXIT_FAILURE);
@@ -248,13 +294,14 @@ int main(int argc, char *argv[])
      * the structure.
      */
 
+    struct sockaddr_un name;
     memset(&name, 0, sizeof(name));
 
     /* Bind socket to socket name. */
 
     name.sun_family = AF_UNIX;
     strncpy(name.sun_path, SOCKET_NAME, sizeof(name.sun_path) - 1);
-    ret = unlink(SOCKET_NAME);
+    int ret = unlink(SOCKET_NAME);
     if (ret == -1 && errno != ENOENT) {
         printf("unlink %s", strerror(errno));
         perror("unlink");
@@ -281,6 +328,9 @@ int main(int argc, char *argv[])
 
     /* This is the main loop for handling connections. */
 
+    int data_socket {0};
+    u8 r_buffer[26];
+    u8 s_buffer[26];
     for (;;) {
 
         /* Wait for incoming connection. */
