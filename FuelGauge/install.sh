fde5bdaf9b2f61d70a8a9cb6731cb1816e6bb8f6
Horst Dumcke <hdumcke@marlysys.com>
2023-01-02 16:02:01 +0100

    updated FuelGauge with information from ESP32

--- FuelGauge/install.sh
+++ FuelGauge/install.sh
@@ -8,14 +8,7 @@ set -x
 BASEDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 
-make clean
-make
-make install
-
 sudo cp battery_monitor /usr/bin/
 sudo sed -i "s|BASEDIR|$BASEDIR/..|" /usr/bin/battery_monitor
 sudo cp battery_monitor.service /lib/systemd/system/
 sudo systemctl enable  battery_monitor.service
-
-dtc i2c4.dts > i2c4.dtbo
-sudo cp i2c4.dtbo /boot/firmware/overlays/
